92c45a1e74b88a9841d81cf5ddfa47f35fb46899
Jihye Eom <jeom327@outlook.com>
2019-01-29 18:03:39 +0900

    Update appcenter-post-build.sh

--- src/VSACReactNative/appcenter-post-build.sh
+++ src/VSACReactNative/appcenter-post-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Repro-> BUILD_SOURCEBRANCHNAME:Branch $BUILD_SOURCEBRANCHNAME"
-echo "Workaround -> Branch" $BUILD_SOURCEBRANCHNAME | sed 's/refs\/heads\///'
+echo "Repro-> BUILD_SOURCEBRANCHNAME: $BUILD_SOURCEBRANCHNAME"
+echo "Workaround -> BUILD_SOURCE BRANCH with some parsing" $BUILD_SOURCEBRANCH | sed 's/refs\/heads\///'
